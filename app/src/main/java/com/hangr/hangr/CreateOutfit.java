d9e178bfacfcd737f8183c8afe2f4f4bfe850dc8
Aalasa Fidler <aalasa.fidler@ucdconnect.ie>
2018-10-24 10:05:01 +0100

    3 carousels now on CreateOutfit

--- app/src/main/java/com/hangr/hangr/CreateOutfit.java
+++ app/src/main/java/com/hangr/hangr/CreateOutfit.java
@@ -8,29 +8,57 @@ import android.widget.TextView;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
-import org.w3c.dom.Text;
 
 public class CreateOutfit extends AppCompatActivity{
 
-    CarouselView tops_carousel;
+    CarouselView tops_carousel, bottoms_carousel, shoes_carousel;
 
     int[] tops_images = {R.drawable.ic_launcher_background};
+    int[] bottoms_images = {R.drawable.ic_launcher_background};
+    int[] shoes_images = {R.drawable.ic_launcher_background};
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_outfit);
 
+        // Dealing with the tops
         TextView tops_text = findViewById(R.id.tops_text);
         tops_carousel = (CarouselView) findViewById(R.id.tops_carousel);
         tops_carousel.setPageCount(tops_images.length);
         tops_carousel.setImageListener(tops_Listener);
+
+        // Dealing with the bottoms
+        TextView bottoms_text = findViewById(R.id.bottoms_text);
+        bottoms_carousel = (CarouselView) findViewById(R.id.bottoms_carousel);
+        bottoms_carousel.setPageCount(bottoms_images.length);
+        bottoms_carousel.setImageListener(bottoms_Listener);
+
+        // Dealing with the shoes
+        TextView shoes_text = findViewById(R.id.shoes_text);
+        shoes_carousel = (CarouselView) findViewById(R.id.shoes_carousel);
+        shoes_carousel.setPageCount(shoes_images.length);
+        shoes_carousel.setImageListener(shoes_Listener);
     }
 
     ImageListener tops_Listener = new ImageListener() {
         @Override
         public void setImageForPosition(int position, ImageView imageView) {
             imageView.setImageResource(tops_images[position]);
+        }
+    };
+
+    ImageListener bottoms_Listener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(bottoms_images[position]);
+        }
+    };
+
+    ImageListener shoes_Listener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(shoes_images[position]);
         }
     };
 }
