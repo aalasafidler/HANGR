be73d1e7c1e5fc19817a9c5e14f626dada059118
dillonfr <dillonfriel@gmail.com>
2018-11-19 16:19:51 +0000

    saving outfits image to new folder

--- app/src/main/java/com/hangr/hangr/CreateOutfit.java
+++ app/src/main/java/com/hangr/hangr/CreateOutfit.java
@@ -4,22 +4,35 @@ import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
 public class CreateOutfit extends AppCompatActivity{
-
+    Button saveOutfitButton;
     CarouselView tops_carousel, bottoms_carousel, shoes_carousel;
+    Bitmap selectedTop, selectedBottom, selectedShoes, mergedOutfit;
 //    Bitmap picture = BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.hangr.hangr/files/Pictures/Hangr_20181112__140519.jpg");
 
     public static WardrobeItemDatabase wardrobeItemDatabase;
@@ -71,6 +84,57 @@ public class CreateOutfit extends AppCompatActivity{
 
         shoes_carousel.setPageCount(shoes_images.size());
         shoes_carousel.setImageListener(shoes_Listener);
+        shoes_carousel.getCurrentItem();
+
+
+        tops_carousel.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                //Toast.makeText(CreateOutfit.this, "Clicked item: "+ position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(CreateOutfit.this, "Item: " + tops_carousel.getCurrentItem(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottoms_carousel.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(CreateOutfit.this, "Clicked item: "+ position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        shoes_carousel.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(CreateOutfit.this, "Clicked item: "+ position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        saveOutfitButton = findViewById(R.id.save_outfit_button);
+        saveOutfitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CreateOutfit.this, "Top selected: " + tops_carousel.getCurrentItem(), Toast.LENGTH_SHORT).show();
+                selectedTop = tops_images.get(tops_carousel.getCurrentItem());
+                selectedBottom = bottoms_images.get(bottoms_carousel.getCurrentItem());
+                selectedShoes = shoes_images.get(shoes_carousel.getCurrentItem());
+
+                // Initialise the file that the outfit will be saved to
+                File file = getOutfitFile();
+                System.out.println("File: " + file);
+
+                Bitmap mergedOutfit = mergeMultiple(new Bitmap[]{selectedTop, selectedBottom, selectedShoes});
+
+                try (FileOutputStream out = new FileOutputStream(file)) {
+                    mergedOutfit.compress(Bitmap.CompressFormat.JPEG, 100, out); // bmp is your Bitmap instance
+                    // PNG is a lossless format, the compression factor (100) is ignored
+                    Toast.makeText(CreateOutfit.this, "Saved: "+ file.toString(), Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
 
     }
 
@@ -146,6 +210,49 @@ public class CreateOutfit extends AppCompatActivity{
         public void setImageForPosition(int position, ImageView imageView) {
             imageView.setImageBitmap(shoes_images.get(position));
         }
+
     };
+
+    private File getOutfitFile() {
+        File folder = getExternalFilesDir(Environment.DIRECTORY_PICTURES + "/Outfits");
+        System.out.println("Folder: " + folder);
+
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd__HHmmss").format(new Date());
+        String imageFileName = "HangrOutfit_" + timeStamp + ".jpg";
+
+        File image_file = new File(folder, imageFileName);
+        return image_file;
+    }
+
+    private Bitmap mergeMultiple(Bitmap[] parts){
+
+        Bitmap result = Bitmap.createBitmap(parts[0].getWidth(), parts[0].getHeight() + parts[1].getHeight() + parts[2].getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(result);
+        Paint paint = new Paint();
+        int top = 0;
+
+        canvas.drawBitmap(parts[0], 0, 0, paint);
+
+        System.out.println("Height of 0: " + parts[0].getHeight());
+        System.out.println("Height of 1: " + parts[1].getHeight());
+        System.out.println("Height of 2: " + parts[2].getHeight());
+
+        top += parts[0].getHeight();
+
+        canvas.drawBitmap(parts[1], 0, top, paint);
+
+        top += parts[1].getHeight();
+
+        canvas.drawBitmap(parts[2], 0, top, paint);
+        //canvas.drawBitmap(parts[2], 0, parts[0].getHeight() + parts[1].getHeight(), paint);
+//        for (int i = 0; i < parts.length; i++) {
+//            canvas.drawBitmap(parts[i], 0, parts[i].getHeight() * (i / 2), paint); //left: parts[i].getWidth()
+//        }
+        return result;
+    }
 
 }
